570baec22f9c1ef0de1eb26f49f52e8eb8d0befe
zahidkaraca <36535478+zahidkaraca@users.noreply.github.com>
2021-06-14 21:59:13 +0300

    Case:11-12-13: öğrenci Listesi
    öğrenci Listesi düzenleme işlemi ve kontrolü yapılmıştır

---
+++ images/öğrenci_listesi.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YurtOtomasyonu
+{
+    public partial class FrmOgrListe : Form
+    {
+        public FrmOgrListe()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmOgrListe_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'yurtOtomasyonuDataSet5.Ogrenci' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.ogrenciTableAdapter.Fill(this.yurtOtomasyonuDataSet5.Ogrenci);
+
+        }
+        int secilen;
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Öğrenci kayıt ekranındaki bilgileri kullanarak öğrenci listesine çift tık yaparak bilgileri görüntülüyoruz
+            secilen = dataGridView1.SelectedCells[0].RowIndex;
+            FrmOgrDuzenle fr = new FrmOgrDuzenle();
+            fr.id = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            fr.ad = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            fr.soyad = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            fr.TC = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            fr.telefon = dataGridView1.Rows[secilen].Cells[4].Value.ToString();
+            fr.dogum = dataGridView1.Rows[secilen].Cells[5].Value.ToString();
+            fr.bolum = dataGridView1.Rows[secilen].Cells[6].Value.ToString();
+            fr.mail = dataGridView1.Rows[secilen].Cells[7].Value.ToString();
+            fr.odaNo = dataGridView1.Rows[secilen].Cells[8].Value.ToString();
+            fr.veliadsoyad = dataGridView1.Rows[secilen].Cells[9].Value.ToString();
+            fr.velitel = dataGridView1.Rows[secilen].Cells[10].Value.ToString();
+            fr.adres = dataGridView1.Rows[secilen].Cells[11].Value.ToString();
+            fr.Show();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
