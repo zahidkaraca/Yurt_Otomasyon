5b53f190fefe8324b0efaec01a4bbb4055fdd1d9
zahidkaraca <36535478+zahidkaraca@users.noreply.github.com>
2021-06-14 21:48:33 +0300

    Case:28 Not ekle
    Not ekleme işlemi tamamlanmıştır.

---
+++ images/NotEkle.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace YurtOtomasyonu
+{
+    public partial class FrmNotEkle : Form
+    {
+        public FrmNotEkle()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKAydet_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Kayıt Yeri Seçin";
+            saveFileDialog1.Filter = "Metin Dosyası | *.txt";
+            saveFileDialog1.InitialDirectory = "c://Notlar";
+            saveFileDialog1.ShowDialog();
+            StreamWriter kaydet = new StreamWriter(saveFileDialog1.FileName);
+            kaydet.WriteLine(richTextBox1.Text);
+            kaydet.Close();
+            MessageBox.Show("Kayıt Başarılı");  
+
+
+        }
+    }
+}
