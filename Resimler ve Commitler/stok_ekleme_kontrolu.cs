53f512ff75eab99b61fed31c072992a4da978d67
zahidkaraca <36535478+zahidkaraca@users.noreply.github.com>
2021-06-21 22:59:58 +0300

    stok ekleme kontrolü yapılmıştır.
    Stok ekleme işinin kodu ile beraber kontrolü de yapılmıştır.

---
+++ Resimler ve Commitler/stok_ekleme_kontrolu.cs
@@ -0,0 +1,24 @@
+private void Kaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                baglanti.Open();
+                SqlCommand komutkaydet = new SqlCommand("insert into A_Blok_Stok (yatak,masa,sandalye,dolap,komodin,Ablok_id) values(@k1,@k2,@k3,@k4,@k5,k6)", baglanti);
+                komutkaydet.Parameters.AddWithValue("@k1", TxtYatakSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k2", TxtMasaSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k3", TxtSandalyeSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k4", TxtDolapSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k5", TxtKomodinSayisi.Text);
+                komutkaydet.Parameters.AddWithValue("@k6", TxtAblokid.Text);
+                
+                komutkaydet.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt Başarılı Bir Şekilde Gerçekleşmiştir");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hata!!! Tekrar Deneyiniz");
+            }
+        }
