e05bf8fa49a75106a997b15d0cf2e8f4408f64e4
zahidkaraca <36535478+zahidkaraca@users.noreply.github.com>
2021-06-22 23:15:16 +0300

    A Blok Stok güncelleme işlemi
    güncelleme işlemi yapılmıştır.

---
+++ Resimler ve Commitler/stok_guncelle.cs
@@ -0,0 +1,10 @@
+baglanti.Open();
+                SqlCommand komut = new SqlCommand("update A_Blok_Stok set yatak=@k1,masa=@k2,sandalye=@k3,dolap=@k4,komodin=@k5 where Ablok_id=@k6", baglanti);
+                komut.Parameters.AddWithValue("@k1", TxtYatakSayisi.Text);
+                komut.Parameters.AddWithValue("@k2", TxtMasaSayisi.Text);
+                komut.Parameters.AddWithValue("@k3", TxtSandalyeSayisi.Text);
+                komut.Parameters.AddWithValue("@k4", TxtDolapSayisi.Text);
+                komut.Parameters.AddWithValue("@k5", TxtKomodinSayisi.Text);
+                komut.Parameters.AddWithValue("@k6", TxtAblokid.Text);
+                komut.ExecuteNonQuery();
+                baglanti.Close();
